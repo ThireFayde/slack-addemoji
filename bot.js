a942130ed64b47a3a34743b182e66653b11999c0
ThireFayde <thire.jp@gmail.com>
2018-11-08 12:56:11 +0900

    非同期処理に変更

--- bot.js
+++ bot.js
@@ -25,41 +25,53 @@ var bot = controller.spawn({
 controller.hears(['アンケート】'], 'direct_message,direct_mention,mention,ambient', function (bot, message) {
 
     function hai(){
-        bot.api.reactions.add({
-            timestamp: message.ts,
-            channel: message.channel,
-            name: 'hai-',
-        }, function (err, res) {
-            if (err) {
-                bot.botkit.log('Failed to add emoji reaction :(', err);
-            }
+        return new Promise(function(resolve, reject){
+            bot.api.reactions.add({
+                timestamp: message.ts,
+                channel: message.channel,
+                name: 'hai-',
+            }, function (err, res) {
+                if (err) {
+                    bot.botkit.log('Failed to add emoji reaction :(', err);
+                }else{
+                    resolve('hai')
+                }
+            });
+            
         });
     }
     function iie(){
-        bot.api.reactions.add({
-            timestamp: message.ts,
-            channel: message.channel,
-            name: 'iie',
-        }, function (err, res) {
-            if (err) {
-                bot.botkit.log('Failed to add emoji reaction :(', err);
-            }
+        return new Promise(function(resolve, reject){
+            bot.api.reactions.add({
+                timestamp: message.ts,
+                channel: message.channel,
+                name: 'iie',
+            }, function (err, res) {
+                if (err) {
+                    bot.botkit.log('Failed to add emoji reaction :(', err);
+                }else{
+                    resolve('iie')
+                }
+            });
         });
     }
     function sinngi(){
-        bot.api.reactions.add({
-            timestamp: message.ts,
-            channel: message.channel,
-            name: 'sinngi',
-        }, function (err, res) {
-            if (err) {
-                bot.botkit.log('Failed to add emoji reaction :(', err);
-            }
+        return new Promise(function(resolve, reject){
+            bot.api.reactions.add({
+                timestamp: message.ts,
+                channel: message.channel,
+                name: 'sinngi',
+            }, function (err, res) {
+                if (err) {
+                    bot.botkit.log('Failed to add emoji reaction :(', err);
+                }else{
+                    resolve('sinngi')
+                }
+            });
         });
     }
 
-    var promise = Promise.resolve();
-    promise
+    Promise.resolve()
         .then(hai)
         .then(iie)
         .then(sinngi);
@@ -68,30 +80,37 @@ controller.hears(['アンケート】'], 'direct_message,direct_mention,mention,
 controller.hears(['募集】'], 'direct_message,direct_mention,mention,ambient', function (bot, message) {
 
     function sanka(){
-        bot.api.reactions.add({
-            timestamp: message.ts,
-            channel: message.channel,
-            name: 'sanka',
-        }, function (err, res) {
-            if (err) {
-                bot.botkit.log('Failed to add emoji reaction :(', err);
-            }
+        return new Promise(function(resolve, reject){
+            bot.api.reactions.add({
+                timestamp: message.ts,
+                channel: message.channel,
+                name: 'sanka',
+            }, function (err, res) {
+                if (err) {
+                    bot.botkit.log('Failed to add emoji reaction :(', err);
+                }else{
+                    resolve('sanka')
+                }
+            });
         });
     }
     function husanaka(){
-        bot.api.reactions.add({
-            timestamp: message.ts,
-            channel: message.channel,
-            name: 'husanka',
-        }, function (err, res) {
-            if (err) {
-                bot.botkit.log('Failed to add emoji reaction :(', err);
-            }
+        return new Promise(function(resolve, reject){
+            bot.api.reactions.add({
+                timestamp: message.ts,
+                channel: message.channel,
+                name: 'husanka',
+            }, function (err, res) {
+                if (err) {
+                    bot.botkit.log('Failed to add emoji reaction :(', err);
+                }else{
+                    resolve('husanka')
+                }
+            });
         });
     }
 
-    var promise = Promise.resolve();
-    promise
+    Promise.resolve()
         .then(sanka)
         .then(husanaka);
 });
